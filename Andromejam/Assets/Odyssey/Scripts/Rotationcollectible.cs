6094dd1514152237a01394d7311f30dd653df0b2
Unknown <gamer.florentin@gmail.com>
2018-03-24 10:29:12 +0100

    new rotation for blackhole

--- Andromejam/Assets/Odyssey/Scripts/Rotationcollectible.cs
+++ Andromejam/Assets/Odyssey/Scripts/Rotationcollectible.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotationcollectible : MonoBehaviour{
-
-    public void Start()
-    {
-        
-    }
+public class Rotationcollectible : MonoBehaviour {
 
     //Update is called every frame
     void Update()
     {
         //Rotate thet transform of the game object this is attached to by 45 degrees, taking into account the time elapsed since last frame.
-        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+        GetComponent<Rigidbody2D>().rotation += 45 * Time.deltaTime;
     }
 }
