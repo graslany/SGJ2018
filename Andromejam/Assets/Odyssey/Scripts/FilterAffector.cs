997106b9fc7d6045e15558611d62ce1678891bd9
Azarias Boutin <azarias.boutin@gmail.com>
2018-03-24 09:37:31 +0100

    Added filter component

---
+++ Andromejam/Assets/Odyssey/Scripts/FilterAffector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FilterAffector : MonoBehaviour {
+
+    public string TagToFilter;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Button>().onClick.AddListener(ApplyFilter);
+	}
+
+
+    void ApplyFilter()
+    {
+        GameObject[] allObjects = GameObject.FindGameObjectsWithTag(TagToFilter);
+        
+        foreach(GameObject o in allObjects)
+        {
+            var renderer = o.GetComponent<SpriteRenderer>();
+            renderer.enabled = !renderer.enabled;
+        }
+    }
+}
