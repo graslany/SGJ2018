8e9d2830a0c4ba02e226344abb4b3ce2e3ed49f3
Yves Grasland <yves.grasland@gmx.fr>
2018-03-24 15:26:17 +0100

    - Début d'implémentation de la fonctionnalité permettant de descendre d'une plateforme - Ajout des sprites

--- Andromejam/Assets/Platformer/Platform.cs
+++ Andromejam/Assets/Platformer/Platform.cs
@@ -4,14 +4,53 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour {
 
+	public bool IsActive { get ; private set ; }
 
-	// Use this for initialization
-	void Start () {
-		
+	private static readonly float deactivationDelay = 0.25f;
+	float? deactivationDate;
+	bool isPlayerInside;
+
+	public void DeactivateUntilPlayerLeaves() {
+		Debug.Log ("Deactivate");
+
+		Collider2D myCollider = GetComponent<Collider2D> ();
+		deactivationDate = Time.time;
+		myCollider.isTrigger = true;
+		IsActive = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	protected virtual void OnEnable() {
+		Collider2D myCollider = GetComponent<Collider2D> ();
+		if (myCollider == null)
+			Debug.LogError ("Il manque un collider à ce game object");
+	}
+
+	protected virtual void Update () {
+		if (deactivationDate.HasValue && Time.time > deactivationDate.Value + deactivationDelay && !isPlayerInside)
+			Reactivate ();
+	}
+
+	protected virtual void OnTriggerEnter() {
+		Debug.Log ("Enter");
+		isPlayerInside = true;
+	}
+
+	protected virtual void OnTriggerStay() {
+		Debug.Log ("Stay");
+		isPlayerInside = true;
+	}
+
+	protected virtual void OnTriggerExit() {
+		isPlayerInside = false;
+		Reactivate ();
+	}
+
+	private void Reactivate() {
+		Debug.Log ("Reactivate");
+
+		Collider2D myCollider = GetComponent<Collider2D> ();
+		deactivationDate = null;
+		myCollider.isTrigger = false;
+		IsActive = true;
 	}
 }
--- Andromejam/Assets/Platformer/PlatformerPlayerController.cs
+++ Andromejam/Assets/Platformer/PlatformerPlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlatformerPlayerController : MonoBehaviour {
@@ -19,8 +20,12 @@ public class PlatformerPlayerController : MonoBehaviour {
 	[Tooltip("Vitesse maximale du joueur")]
 	public float jumpSpeed = 8;
 
-
+	private List<Platform> traversedPlatforms;
 	private Animator animator;
+
+	protected virtual void Awake() {
+
+	}
 
 	void Start () {
 		animator = GetComponent<Animator> ();
@@ -28,19 +33,30 @@ public class PlatformerPlayerController : MonoBehaviour {
 
 	// Update is called once per frame
 	protected virtual void Update () {
+		UpdateAnimation ();
+		UpdateHorizontalMovement ();
+		UpdateVerticalMovement ();
+	}
+
+	/// <summary>
+	/// Met à jour l'animation du joueur.
+	/// </summary>
+	private void UpdateAnimation() {
 		bool isGoingLeft = input.GoLeftCommand.IsActive ();
 		bool isGoingRight = input.GoRightCommand.IsActive ();
 
 		// Animation
 		animator.SetBool ("IsLeftPressed", isGoingLeft);
 		animator.SetBool ("IsRightPressed", isGoingRight);
+	}
 
-		// Est-on sur le sol ?
-		LayerMask mask = LayerMask.GetMask (new string[] { Layers.PlatformsLayerName });
-		Collider2D ground = Physics2D.OverlapBox (playerFeetPosition.position, new Vector2(playerWidth, 0.1f), 0, mask);
-		bool groundedThisFrame = (ground != null);
-
-		// Déplacement horizontal
+	/// <summary>
+	/// Met à jour le déplacement horizontal du joueur.
+	/// </summary>
+	private void UpdateHorizontalMovement() {
+		
+		bool isGoingLeft = input.GoLeftCommand.IsActive ();
+		bool isGoingRight = input.GoRightCommand.IsActive ();
 		float desiredHorizontalSpeed = 0;
 		if (isGoingLeft)
 			desiredHorizontalSpeed = -maxSpeed;
@@ -49,9 +65,51 @@ public class PlatformerPlayerController : MonoBehaviour {
 		Rigidbody2D rBody = GetComponent<Rigidbody2D> ();
 		rBody.velocity = new Vector2 (desiredHorizontalSpeed, rBody.velocity.y);
 
+	}
+
+	/// <summary>
+	/// Met à jour le déplacement vertical du joueur.
+	/// </summary>
+	private void UpdateVerticalMovement() {
+		
+		// Est-on sur le sol ?
+		LayerMask mask = LayerMask.GetMask (new string[] { Layers.PlatformsLayerName });
+		Collider2D ground = Physics2D.OverlapBox (playerFeetPosition.position, new Vector2(playerWidth, 0.1f), 0, mask);
+		bool groundedThisFrame = (ground != null);
+
 		// Saut
 		if (groundedThisFrame && input.JumpCommand.IsRisingEdge()) {
+			Rigidbody2D rBody = GetComponent<Rigidbody2D> ();
 			rBody.velocity = new Vector2 (rBody.velocity.x, jumpSpeed);
 		}
+
+		// Descente
+		if (groundedThisFrame && input.DropCommand.IsRisingEdge()) {
+			Platform[] platformsUnderPlayer = GetPlatformsUnderPlayer (); ground.GetComponent<Platform> ();
+
+			if (platformsUnderPlayer.Length > 0) {
+
+			}
+		}
+	}
+
+	private Platform[] GetPlatformsUnderPlayer() {
+		
+		LayerMask mask = LayerMask.GetMask (new string[] { Layers.PlatformsLayerName });
+		ContactFilter2D filter = new ContactFilter2D();
+		filter.layerMask = mask;
+		Collider2D[] colliders = new Collider2D[5];
+		int collCount = Physics2D.OverlapBox (playerFeetPosition.position, new Vector2(playerWidth, 0.1f), 0, filter, colliders);
+		if (collCount > colliders.Length) {
+			colliders = new Collider2D[collCount];
+			collCount = Physics2D.OverlapBox (playerFeetPosition.position, new Vector2(playerWidth, 0.1f), 0, filter, colliders);
+		}
+
+		return colliders.
+			Take(collCount).
+			Select(coll => coll.GetComponent<Platform>()).
+			Where(p => p != null && p.IsActive).
+			ToArray();
+		
 	}
 }
