bcc589110001542068be73bd1ecd6298ec977818
Unknown <gamer.florentin@gmail.com>
2018-03-24 16:51:03 +0100

    Add scene et menu et masse choses

---
+++ Andromejam/Assets/Menu/script/QuitOnClick.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QuitOnClick : MonoBehaviour
+{
+    public void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Quit);
+    }
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+
+}
