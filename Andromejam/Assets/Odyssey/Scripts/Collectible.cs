e297e8c9223cd72920fe2f77a45a53802e29d9bd
Unknown <gamer.florentin@gmail.com>
2018-03-24 11:11:04 +0100

    change important

---
+++ Andromejam/Assets/Odyssey/Scripts/Collectible.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision on");
+        if (collision.gameObject.tag == "Spaceship")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
