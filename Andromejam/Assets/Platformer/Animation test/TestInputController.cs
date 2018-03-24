609f197082fb01772e22a67b41fbaac1e3fd2502
Yves Grasland <yves.grasland@gmx.fr>
2018-03-24 01:59:07 +0100

    Tests sur les animations

---
+++ Andromejam/Assets/Platformer/Animation test/TestInputController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInputController : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		animator.SetBool ("IsLeftPressed", Input.GetKey(KeyCode.LeftArrow));
+		animator.SetBool ("IsRightPressed", Input.GetKey(KeyCode.RightArrow));
+	}
+}
