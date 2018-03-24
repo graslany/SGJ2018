6f2dec888d187ac68cb4cb8d3e7024cff76c40c2
Azarias Boutin <azarias.boutin@gmail.com>
2018-03-24 13:53:26 +0100

    Progressive text

--- Andromejam/Assets/Odyssey/Scripts/Arrival.cs
+++ Andromejam/Assets/Odyssey/Scripts/Arrival.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Arrival : MonoBehaviour {
 
+    [Tooltip("Text to display when finished")]
+    public GameObject FinalText;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,6 +15,8 @@ public class Arrival : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other)
     {
         GetComponent<ParticleSystem>().Play();
+        if(FinalText != null)
+            FinalText.GetComponent<ProgressiveText>().Run();
 
     }
 }
