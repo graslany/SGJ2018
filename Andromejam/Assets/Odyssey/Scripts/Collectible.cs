91c6a394b495c2062def88789d386c1cafc9d703
Azarias Boutin <azarias.boutin@gmail.com>
2018-03-24 15:31:23 +0100

    New spaceship

--- Andromejam/Assets/Odyssey/Scripts/Collectible.cs
+++ Andromejam/Assets/Odyssey/Scripts/Collectible.cs
@@ -7,7 +7,7 @@ public class Collectible : MonoBehaviour {
     private bool collectAnimation = false;
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Spaceship")
             CollidedWithSpaceship();
