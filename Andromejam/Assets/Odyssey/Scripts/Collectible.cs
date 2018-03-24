9ade7aa6a23cede137becf4e4baacd3bdbeb281c
Azarias Boutin <azarias.boutin@gmail.com>
2018-03-24 14:00:17 +0100

    Small updates

--- Andromejam/Assets/Odyssey/Scripts/Collectible.cs
+++ Andromejam/Assets/Odyssey/Scripts/Collectible.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour {
 
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Spaceship")
-        {
-            Destroy(this.gameObject);
-        }
+            CollidedWithSpaceship();
     }
 
     // Use this for initialization
@@ -22,5 +22,12 @@ public class Collectible : MonoBehaviour {
     void Update()
     {
 
+    }
+
+    private void CollidedWithSpaceship()
+    {
+        Destroy(GetComponent<Rigidbody2D>());
+        Destroy(GetComponent<BoxCollider2D>());
+        GetComponent<ParticleSystem>().Stop();
     }
 }
