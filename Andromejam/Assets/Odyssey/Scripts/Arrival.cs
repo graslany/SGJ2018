5f1836fa1ae6db3217f3cce89eda4019c2eb9e9f
Azarias Boutin <azarias.boutin@gmail.com>
2018-03-24 13:24:30 +0100

    Particle and arrival

---
+++ Andromejam/Assets/Odyssey/Scripts/Arrival.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrival : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collided !");
+    }
+}
